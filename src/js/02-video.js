d806bd3e70983e9e80f763e13c5fff632cb50b26
Alex-Sorokolit <sorokolital@gmail.com>
2022-09-29 10:23:38 +0300

    player done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,39 @@
-import Player from '@vimeo/player';
-console.log(Player);
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// const videoEl = document.querySelector('#vimeo-player');
-// console.log(videoEl);
+// Створюємо константу ключа для localeStorage
+const LOCALSTORAGE_KEY = 'current-play-time'
+
+// Дістаємо плеєер із сторінки
+const playerElements = document.querySelector('#vimeo-player')
+// console.log(playerElements);
+
+// Створюємо об'єкта player із імпортованого VimeoPlayer 
+const player = new VimeoPlayer(playerElements);
+// console.log(player);
+
+
+
+// Відслідкувати час
+const onPlay = function(data) {
+  const time = JSON.stringify(data.seconds)
+  console.log(time);
+  // Зберегти час у localStorage
+localStorage.setItem(LOCALSTORAGE_KEY,time)  
+};
+// Метод .on це метод бібліотеки vimeo він приймає 
+// on(event: string, callback: function)
+// на callback-функцію додаємо throttle щоб дані записувались з інтервалом
+player.on('timeupdate', throttle(onPlay,1000));
+
+
+
+// Перезавантаження сторінки виконати метод setCurrentTime()
+// дістаємо значення часу із localeStorage
+const initPage = localStorage.getItem(LOCALSTORAGE_KEY)
+console.log('дані із localeStorage',initPage);
+
+// Встановлюємо час
+if (initPage) {
+  player.setCurrentTime(initPage)
+}
